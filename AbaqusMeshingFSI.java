76960cadf9902e1153c52c8cebef9281be7f974c
cj8q5 <cj8q5@mail.missouri.edu>
2014-05-07 20:48:38 -0500

    20140507_StarSaveName
    Changed the save name for stacks in the java macro.

--- AbaqusMeshingFSI.java
+++ AbaqusMeshingFSI.java
@@ -425,10 +425,21 @@ public class AbaqusMeshingFSI extends StarMacro
 		/**-----------------------------------------------------------------------------------------------------------------------------------------------------
 			SAVING AND RUNNING NODE */		
 		// Saving and running the simulation
-		String saveLocation = currentDirectory + File.separator + 
-			"FSI_" + (int)Math.abs(initialVel[1]) + "_" + plateGeometry + '_' +
-			(int)(plateHeight/0.0254*1000) + "_" + (int)(smallChannelHeight/0.0254*1000) + 
-			"_" + (int)(largeChannelHeight/0.0254*1000) +".sim";
+		String saveLocation;
+		if(numOfPlates == 1)
+		{
+			saveLocation = currentDirectory + File.separator + 
+					"FSI_" + (int)Math.abs(initialVel[1]) + "_" + plateGeometry + '_' +
+					(int)(plateHeight/0.0254*1000) + "_" + (int)(smallChannelHeight/0.0254*1000) + 
+					"_" + (int)(largeChannelHeight/0.0254*1000) +".sim";
+		}
+		else
+		{
+			saveLocation = currentDirectory + File.separator + 
+					"FSI_" + (int)Math.abs(initialVel[1]) + "_" + plateGeometry + '_' +
+					(int)(plateHeight/0.0254*1000) + "_" + (int)(smallChannelHeight/0.0254*1000) + 
+					"_" + numOfPlates + "_" + plateGeometry + "_Plate_Stack" +".sim";
+		}
 		activeSim.saveState(saveLocation);
 		AbaqusCoSimulation coSim = (AbaqusCoSimulation) activeSim.get(CoSimulationManager.class).getCoSimulation("Abaqus Co-Simulation 1");
 		//activeSim.getSimulationIterator().run();
